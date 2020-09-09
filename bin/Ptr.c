8a36f73a3b4bbd96b66a13d0a6b1d3165bd6a1fb
DavideC03 <davidecastellani@castellanidavide.it>
2020-09-09 08:24:49 +0200

    v1.0

--- bin/Ptr.c
+++ bin/Ptr.c
@@ -16,7 +16,12 @@
 #include <stdio.h>
 
 int main() {
-	// printf() displays the string inside quotation
-	printf("Ptr");
+	int a;
+	int *aPtr = &a;
+
+	printf("The address of a is %p\nThe value of aPtris %p", &a, aPtr);
+	printf("\n\nThe value of a is %d\nThe value of *aPtris %d", a, *aPtr);
+	printf("\n\nShowing that * and & are complements of each other \n&*aPtr = %p\n", *&aPtr);
+
 	return 0;
 }
