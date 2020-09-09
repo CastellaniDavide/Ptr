93473b279fba70cfd03b144299e8a521970bf4fd
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-09-09 08:15:28 +0200

    Created bin/template.c

---
+++ bin/Ptr.c
@@ -0,0 +1,22 @@
+/**
+ * @file Ptr.cpp
+ *
+ * @version 01.01 2020-08-15
+ *
+ * @brief Puntators example in c
+ *
+ * @ingroup Ptr
+ * (Note: this needs exactly one @defgroup somewhere)
+ *
+ * @author Castellani Davide
+ *
+ * Contact: contacts@castellanidavide.it
+ *
+ */
+#include <stdio.h>
+
+int main() {
+	// printf() displays the string inside quotation
+	printf("Ptr");
+	return 0;
+}
